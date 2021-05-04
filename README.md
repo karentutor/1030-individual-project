119de7227a7eeb8317395709495dcdc194570c7c
Karen K <karentutor1@gmail.com>
2021-05-03 19:33:21 -0700

    Final

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 https://github.com/karentutor/1030-individual-project
 Register a user<br />
 Login<br />
-Contact us is viewable as entries<br />
 Routes are protected<br />
 Shows or displays navs based on login status<br />
+CRUD projects (portfolio) and accomplishments (resume)
 Special thanks for George Lomidze and Bootstrap [various] from which the design of this website html / css was made.<br />
+
+SQL LINES MAY BE FOUND AT<br />
+backend/controllers/accomplishment : 29, 44, 53, 67, 116, 165,166
+backend/controllers/auth : 60 
+backend/controllers/project: 29, 43, 53, 66, 114, 163, 164 
+backend/controllers/type: 13  
+backend/controllers/user: 12, 41, 48, 56, 126, 140  
 # 1030-individual-project
--- sql.sql
+++ sql.sql
@@ -16,13 +16,44 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `portfolio`
+-- Table structure for table `accomplishments`
 --
 
-DROP TABLE IF EXISTS `portfolio`;
+DROP TABLE IF EXISTS `accomplishments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `portfolio` (
+CREATE TABLE `accomplishments` (
+  `_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `completed` varchar(50) DEFAULT NULL,
+  `type` int DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `postedBy` varchar(45) NOT NULL,
+  `role` enum('subscriber','admin') NOT NULL,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accomplishments`
+--
+
+LOCK TABLES `accomplishments` WRITE;
+/*!40000 ALTER TABLE `accomplishments` DISABLE KEYS */;
+INSERT INTO `accomplishments` VALUES (6,'Karen is Funny!','Karen is oh so funny -- she is a funny bunny that can be very very punny...','2021-05-12',1,'2021-05-04 02:11:18','2021-05-04 02:11:18','10','admin'),(7,'Karen is an awesome coder','She is so awesome that she finishes all her work yesterday!  Hire her  - dial and be done -- yesterday!','2021-05-16',1,'2021-05-04 02:12:03','2021-05-04 02:12:03','10','admin');
+/*!40000 ALTER TABLE `accomplishments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project` (
   `_id` int unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
   `description` varchar(250) NOT NULL,
@@ -33,16 +64,17 @@ CREATE TABLE `portfolio` (
   `postedBy` varchar(45) NOT NULL,
   `role` enum('subscriber','admin') NOT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `portfolio`
+-- Dumping data for table `project`
 --
 
-LOCK TABLES `portfolio` WRITE;
-/*!40000 ALTER TABLE `portfolio` DISABLE KEYS */;
-/*!40000 ALTER TABLE `portfolio` ENABLE KEYS */;
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES (16,'Delightful Website','A feast for the senses! Vibrant for the eyes ... a luscious delightful wondrous experience that will leave you utterly breathless!\r\nCompleted yesterday... of course!','1','2021-05-09','2021-05-04 02:13:14','2021-05-04 02:13:14','10','admin'),(17,'Gorgoues Thing a Mah Jig','Another delightful project completed ----- yes,,,, you guessed it yesterday!','1','2021-05-11','2021-05-04 02:15:14','2021-05-04 02:15:14','10','admin'),(19,'The Social Webstie','The most social website ever! You will improve your confidence ...be giddy with job .. and become more attractive ... and of course this website was completed ... yup! Yesterday!','2','2021-05-12','2021-05-04 02:18:01','2021-05-04 02:18:01','10','admin');
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -84,7 +116,7 @@ CREATE TABLE `users` (
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `role` varchar(30) NOT NULL DEFAULT 'subscriber',
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +125,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (9,'karen','karen@karen.com','7c39bfe7489a48b29257f702443a4a4fe7cf6c8216e2df043c33d0f5b8db867fabe37f93095e3fa7c04d9c5ac2760bd33585b136b65c21288e7254ce0d328239','2021-04-27 20:07:17','subscriber');
+INSERT INTO `users` VALUES (9,'karen','karen@karen.com','7c39bfe7489a48b29257f702443a4a4fe7cf6c8216e2df043c33d0f5b8db867fabe37f93095e3fa7c04d9c5ac2760bd33585b136b65c21288e7254ce0d328239','2021-04-27 20:07:17','subscriber'),(10,'admin','admin@admin.com','7c39bfe7489a48b29257f702443a4a4fe7cf6c8216e2df043c33d0f5b8db867fabe37f93095e3fa7c04d9c5ac2760bd33585b136b65c21288e7254ce0d328239','2021-05-03 16:42:56','admin'),(11,'jack','jack@jack.com','7c39bfe7489a48b29257f702443a4a4fe7cf6c8216e2df043c33d0f5b8db867fabe37f93095e3fa7c04d9c5ac2760bd33585b136b65c21288e7254ce0d328239','2021-05-03 21:46:10','subscriber');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -106,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-02  9:41:55
+-- Dump completed on 2021-05-03 19:20:04
